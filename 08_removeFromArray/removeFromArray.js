dc3fbabee581eb5c2a494e1de18a5c276bab24ba
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2025-09-02 18:47:17 +0800

    Finish all exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function removeElement(arr, ...element) {
+    return arr.filter(item => !element.includes(item));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function getSum(num1, num2) {
+    let num = [];
+    if (num2 > num1 && num1 >= 0 && num2 >= 0) {
+        if ((Number.isInteger(num1)) && (Number.isInteger(num2))) {
+            for (let i=num1; i <= num2; i++) {
+                num.push(i);
+            }
+        } else {
+            return 'ERROR';
+        }
+            return num.reduce((accumulator, current) => accumulator + current, 0);
+    } else if (num1 > num2 && num1 >= 0 && num2 >= 0) {
+        if ((Number.isInteger(num1)) && (Number.isInteger(num2))) {
+            for (let i=num2; i <= num1; i++) {
+                num.push(i);
+            }
+        } else {
+            return 'ERROR';
+        }
+            return num.reduce((accumulator, current) => accumulator + current, 0);
+    } else {
+        return 'ERROR' ;
+    }
 };
 
 // Do not edit below this line
